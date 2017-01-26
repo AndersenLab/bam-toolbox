e3a58867159976272786843aeb2e914cbf2639c1
Daniel Cook <danielecook@gmail.com>
2017-01-25 20:38:41 -0600

    fix output

--- bam/coverage.py
+++ bam/coverage.py
@@ -16,10 +16,53 @@ from docopt import docopt
 from collections import OrderedDict
 from clint.textui import colored, indent, puts_err
 import os
-from bam.output import *
 import re
 from pysam import AlignmentFile
 from subprocess import Popen, PIPE
+
+from datetime import datetime
+from collections import OrderedDict
+
+class output_line:
+    """
+        Entity-Attributes-Value Model
+    """
+    header_out = False
+    def __init__(self, entity, attributes, value, header = False):
+        self.entity = entity
+        if type(attributes) in [dict, OrderedDict]:
+            attributes = [k + "=" + str(v) for k,v in attributes.items()]
+        elif type(attributes) != list:
+            attributes = [attributes]
+        self.attributes = attributes
+        self.value = value
+        if not output_line.header_out and header:
+            print("bam\tcontig\tstart\tend\tproperty\tvalue")
+            output_line.header_out = True
+
+
+    def __setattr__(self, name, value):
+        # Value is attribute
+        if name == "add_attr" or name == "set_attr":
+            if type(value) in [dict, OrderedDict]:
+                value = [k + "=" + v for k,v in value.items()]
+            elif type(value) != list:
+                value = [value]
+            if name == "add_attr":
+                self.__dict__["attributes"].extend(value)
+            else:
+                self.__dict__["attributes"] = value
+        else:
+            self.__dict__[name] = value
+
+
+    def __repr__(self):
+        attributes = '\t'.join(map(str,[x.split("=")[1] for x in self.attributes]))
+        out = [self.entity, attributes, self.value]
+        output = map(str, out)
+        return '\t'.join(output)
+       
+
 
 
 def get_contigs(bam):
